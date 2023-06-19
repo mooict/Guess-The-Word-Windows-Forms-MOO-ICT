ec824e79f4f3310124512e37a75cb05b78b46a79
anharali00 <44951121+anharali00@users.noreply.github.com>
2023-06-19 10:14:35 +0100

    Add project files.

---
+++ Guess The Word Windows Forms MOO ICT/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Guess_The_Word_Windows_Forms_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblGussed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(240, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Guess The Word";
+            // 
+            // lblWord
+            // 
+            this.lblWord.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblWord.ForeColor = System.Drawing.Color.White;
+            this.lblWord.Location = new System.Drawing.Point(240, 118);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(274, 52);
+            this.lblWord.TabIndex = 1;
+            this.lblWord.Text = "label2";
+            this.lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(259, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(255, 46);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyIsPressed);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblInfo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblInfo.Location = new System.Drawing.Point(240, 280);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(274, 52);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Words: 0 of 0";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGussed
+            // 
+            this.lblGussed.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblGussed.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblGussed.Location = new System.Drawing.Point(520, 203);
+            this.lblGussed.Name = "lblGussed";
+            this.lblGussed.Size = new System.Drawing.Size(274, 47);
+            this.lblGussed.TabIndex = 1;
+            this.lblGussed.Text = "Guessed: 0 times";
+            this.lblGussed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblGussed);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Name = "Form1";
+            this.Text = "Guess The Word Game MOO ICT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lblWord;
+        private TextBox textBox1;
+        private Label lblInfo;
+        private Label lblGussed;
+    }
+}
---
+++ Guess The Word Windows Forms MOO ICT/Form1.cs
@@ -0,0 +1,66 @@
+namespace Guess_The_Word_Windows_Forms_MOO_ICT
+{
+
+    // Made by MOO ICT
+    // For educational purpose only
+    public partial class Form1 : Form
+    {
+
+        List<string> words = new List<string>();
+        string newText;
+        int i = 0;
+        int guessed = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Setup();
+        }
+
+        private void KeyIsPressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (words[i].ToLower() == textBox1.Text.ToLower())
+                {
+                    if (i < words.Count - 1)
+                    {
+                        MessageBox.Show("Correct!", "Moo Says: ");
+                        textBox1.Text = "";
+                        i += 1;
+                        newText = Scramble(words[i]);
+                        lblWord.Text = newText;
+                        lblInfo.Text = "Words: " + (i + 1) + " of " + words.Count;
+                        guessed = 0;
+                        lblGussed.Text = "Guessed: " + guessed + " times.";
+                    }
+                    else
+                    {
+                        lblWord.Text = "You Win, Well done";
+                        return;
+                    }    
+                }
+                else
+                {
+                    guessed += 1;
+                    lblGussed.Text = "Guessed: " + guessed + " times.";
+                }
+                e.Handled = true;
+            }
+        }
+
+        private void Setup()
+        {
+            words = File.ReadLines("words.txt").ToList();
+            newText = Scramble(words[i]);
+            lblWord.Text = newText;
+            lblInfo.Text = "Words: " + (i + 1) + " of " + words.Count;
+        }
+
+        private string Scramble(string text)
+        {
+            return new string(text.ToCharArray().OrderBy(x => Guid.NewGuid()).ToArray());
+        }
+    }
+}
